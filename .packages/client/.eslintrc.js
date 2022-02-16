333a8934b4bc9e93b19d02c667e95521215f6bf9
junilHwang <junil.h@kakao.com>
2022-02-16 23:01:05 +0900

    feat: eslint 설정 완료

---
+++ .packages/client/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "no-empty-pattern": "off",
+  },
+};
